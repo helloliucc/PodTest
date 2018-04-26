d340fb843e3087eff73561ca3171a579bcf2db68
liuchangcheng <liuchangcheng@58ganji.com>
2018-04-26 15:34:57 +0800

    init

---
+++ PodTest.framework/Headers/PodTest.h
@@ -0,0 +1,21 @@
+//
+//  PodTest.h
+//  PodTest
+//
+//  Created by liucc on 26/04/2018.
+//  Copyright © 2018 liucc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PodTest.
+FOUNDATION_EXPORT double PodTestVersionNumber;
+
+//! Project version string for PodTest.
+FOUNDATION_EXPORT const unsigned char PodTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PodTest/PublicHeader.h>
+
+#import<PodTest/Test.h>
+
+
---
+++ PodTest.framework/Headers/Test.h
@@ -0,0 +1,15 @@
+//
+//  Test.h
+//  PodTest
+//
+//  Created by liucc on 26/04/2018.
+//  Copyright © 2018 liucc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@property(nonatomic,strong) NSString *name;
+
+@end
